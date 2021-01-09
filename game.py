46fa30e621717bb363fb208e4b649bce0da81673
javasmuse <candacelewis@mac.com>
2021-01-08 18:37:17 -0800

    better

--- game.py
+++ game.py
@@ -10,7 +10,7 @@ def welcome():
     login = ""
     round = 0
     while round < 3 and login != "sassycat":
-        login = input("Login: ").strip().lower()
+        login = input(crayons.cyan("Login: ")).strip().lower()
         round += 1
         if login == "sassycat":
             print("you found the password!")
@@ -23,16 +23,16 @@ def welcome():
 
 def mainframe():
     print("\nYour computer screen displays this message: \n")
-    print(crayons.blue("MAGMA LTD. - Interal System Bulletins\n"))
+    print(crayons.cyan("MAGMA LTD. - Interal System Bulletins\n"))
     print(crayons.red("TOP SECRET - World Domination Plans have been relocated for security. One page has been sent to an agent in each of our affiliated countries.\n"))
     time.sleep(3)
     print("Hmmm, you think, wasn't their a reward for thwarting evil plans? How can I get those? \n Further down the screen you notice: \n")
     time.sleep(3)
-    print(crayons.blue("The travel droid is fully charged.\n"))
+    print(crayons.cyan("The travel droid is fully charged.\n"))
     time.sleep(3)
     print("What would you like to do? \n Enter 1 - get out of here it could be dangerous \n Enter 2 - click on travel droid")
     while True:
-        action = input(crayons.blue("> ")).strip()
+        action = input(crayons.cyan("> ")).strip()
         if action == "1":
             print(
                 "Good thinking those agents could be dangerous. Maybe go play a safer game like Mario. Goodbye")
@@ -42,21 +42,22 @@ def mainframe():
             travelDroid()
             break
         elif action != "2" and action != "1":
-            action = input(crayons.blue("> ")).strip()
+            action = input(crayons.cyan("> ")).strip()
 
 
 def travelDroid():
-    print(crayons.blue("Hello, I'm a travel droid. I travel underground at the speed of light through special MAGMA LTD. designed tunnels. My charge is good for three hours. I'm also able to impersonate a MAGMA LTD. official. I may be useful in recovering the plans.\nSimple use instruction: to Move: type go with a direction, to Get: type get with an object. \ni.e. go Japan OR get jade"))
+    print(crayons.cyan("Hello, I'm a travel droid. I travel underground at the speed of light through special MAGMA LTD. designed tunnels. My charge is good for three hours. I'm also able to impersonate a MAGMA LTD. official. I may be useful in recovering the plans.\n\nSimple use instruction: to Move: type go with a direction, to Get: type get with an object. \ni.e. go Japan OR get jade\n"))
+    time.sleep(3)
 
 def showStatus():
-    print(crayons.blue('--------------------------'))
-    print(crayons.blue('You are in ' + currentLocale))
+    print(crayons.cyan('--------------------------'))
+    print(crayons.cyan('You are in ' + currentLocale))
     # print players inventory
-    print(crayons.blue("Your inventory includes: " + str(inventory)))
+    print(crayons.cyan("Your inventory includes: " + str(inventory)))
     # print an item if there is one in the room
     if "item" in locales[currentLocale]:
-        print(crayons.blue("You see a " + locales[currentLocale]['item']))
-    print(crayons.blue('--------------------------'))
+        print(crayons.cyan("You see a " + locales[currentLocale]['item']))
+    print(crayons.cyan('--------------------------'))
 
 
 # PLAYERS INVENTORY
@@ -97,7 +98,7 @@ while True:
     # GET PLAYERS ACTION CHOICE
     move = ''
     while move == '':
-        move = input(crayons.blue('> '))
+        move = input(crayons.cyan('> '))
 
     # split allow items to have a space - it removes the space and takes each word as an element, we want the secone one or idx 1
     move = move.lower().split(" ", 1)
@@ -110,7 +111,7 @@ while True:
             currentLocale = locales[currentLocale][move[1]]
         # if there is no portal to the entered locale
         else:
-            print(crayons.blue("Sorry that is not an option. The tunnels are amazing, but the lines are limited as yet."))
+            print(crayons.cyan("Sorry that is not an option. The tunnels are amazing, but the lines are limited as yet."))
 
     # if they type 'get' first
     if move[0] == 'get':
@@ -119,15 +120,15 @@ while True:
             # add the item to their inventory
             inventory += [move[1]]
             # display a helpful message
-            print(crayons.blue("Got it! The "))
+            print(crayons.cyan("\nGot it! All the gifts from the agents inclue a page of the plans! "))
             # delete the item from the room
             del locales[currentLocale]['item']
         # otherwise if the item isn't there to get
         else:
             # tell them they can't get it
-            print(crayons.blue(f"The {move[1]} is not here."))
+            print(crayons.cyan(f"The {move[1]} is not here."))
 
         # Define how a player can win
     if len(inventory) == 5:
-        print(crayons.green("You Win - You saved the world from MAGMA LTD. dominiation!!! :) :) :) :) "))
+        print(crayons.magenta("\nYou Win - You saved the world from MAGMA LTD. dominiation!!! :) :) :) :) \n"))
         break
